4e55e5ca25319ef5e0e94f99a7a9fc3cb69f8aee
FuhuaChen <874166712@qq.com>
2018-07-17 18:41:25 +0800

    添加技能模块

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,18 @@ let app = new Vue({
             birthday: '1992年7月',
             jobTitle: '前端工程师',
             email: '874166712@qq.com',
-            phone: '18060912511'
+            phone: '18060912511',
+            skills:[
+                {name: '请填写技能名称',description:'请填写技能描述'},
+                {name: '请填写技能名称',description:'请填写技能描述'},
+                {name: '请填写技能名称',description:'请填写技能描述'},
+                {name: '请填写技能名称',description:'请填写技能描述'}
+            ],
+            projects:[]
+        },
+        currentUser:{
+            objectId:'',
+            email:''
         },
         signUp:{
             email:'',
@@ -21,37 +32,56 @@ let app = new Vue({
         }
     },
     methods: {
+        hasLogin(){
+            return !!this.currentUser.objectId
+        },
+        getResume(){
+            let query = new AV.Query('User')
+            query.get(this.currentUser.objectId).then((user) => {
+                this.resume = user.toJSON().resume
+            }, (error) => {
+            })
+        },
         saveResume(){
-            let {id} = AV.User.current()
-            let user = AV.Object.createWithoutData('User', id)
+            let {objectId} = AV.User.current().toJSON()
+            let user = AV.Object.createWithoutData('User', objectId)
             user.set('resume', this.resume)
             user.save()
+            alert('保存成功')
         },
         onLogout(){
             AV.User.logOut()
             alert('注销成功')
-            let currentUser = AV.User.current()
+            window.location.reload()
         },
         onSignUp(){
             let user = new AV.User()
             user.setUsername(this.signUp.email)
             user.setPassword(this.signUp.password)
             user.setEmail(this.signUp.email)
-            user.signUp().then(function (user) {
+            user.signUp().then((user) => {
                 alert('注册成功')
-            }, function (error) {
+                let User = user.toJSON()
+                this.currentUser.objectId = User.objectId
+                this.currentUser.email = User.email
+                this.signUpVisible = false
+            },(error) => {
                 console.log(error.code)
                 if(error.code === 125){
                     alert('邮箱错误')
                 }else if(error.code === 203){
-                    alert('邮箱已被注册')
+                    alert('邮箱已被占用')
                 }
             })
         },
         onLogin(){
-            AV.User.logIn(this.login.email, this.login.password).then(function (user) {
-
-            }, function (error) {
+            AV.User.logIn(this.login.email, this.login.password).then((user) => {
+                let User = user.toJSON()
+                this.currentUser.objectId = User.objectId
+                this.currentUser.email = User.email
+                this.loginVisible = false
+                window.location.reload()
+            },(error) => {
                 console.log(error.code)
                 if(error.code === 211){
                     alert('邮箱不存在')
@@ -61,7 +91,19 @@ let app = new Vue({
             })
         },
         onEdit(key, value) {
-            this.resume[key] = value
+            let regex = /\[(\d+)\]/g
+            key = key.replace(regex,(match,number) => {
+                return '.' + number
+            })
+            keys = key.split('.')
+            let result = this.resume
+            for(let i=0;i<keys.length;i++){
+                if(i===keys.length-1){
+                    result[keys[i]] = value
+                }else{
+                    result = result[keys[i]]
+                }
+            }
         },
         onClickSave(){
             let currentUser = AV.User.current()
@@ -71,6 +113,18 @@ let app = new Vue({
             else {
                 this.saveResume()
             }
+        },
+        addSkill(){
+            this.resume.skills.push({name: '请填写技能名称',description:'请填写技能描述'})
+        },
+        removeSkill(index){
+            this.resume.skills.splice(index,1)
         }
     }
 })
+
+let currentUser = AV.User.current()
+if(currentUser){
+    app.currentUser = currentUser.toJSON()
+    app.getResume()
+}
