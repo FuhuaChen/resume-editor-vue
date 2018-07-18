b087b3ab1f9f53b0f11bdf3f71eeb548e20ff963
FuhuaChen <874166712@qq.com>
2018-07-18 13:58:36 +0800

    添加项目模块

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,10 @@ let app = new Vue({
                 {name: '请填写技能名称',description:'请填写技能描述'},
                 {name: '请填写技能名称',description:'请填写技能描述'}
             ],
-            projects:[]
+            projects:[
+                {name:'请填写项目名称',link:'https://github.com/FuhuaChen',keywords:'清填写关键词',description:'清填写项目描述'},
+                {name:'请填写项目名称',link:'https://github.com/FuhuaChen',keywords:'清填写关键词',description:'清填写项目描述'}
+            ]
         },
         currentUser:{
             objectId:'',
@@ -119,6 +122,12 @@ let app = new Vue({
         },
         removeSkill(index){
             this.resume.skills.splice(index,1)
+        },
+        addProject(){
+            this.resume.projects.push({name:'请填写项目名称',link:'https://github.com/FuhuaChen',keywords:'清填写关键词',description:'清填写项目描述'})
+        },
+        removeProject(index){
+            this.resume.projects.splice(index,1)
         }
     }
 })
